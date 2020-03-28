246d826713671a8a315008232a6ab05eb927828b
mak <beherapapu44@gmail.com>
2020-03-28 12:51:39 +0530

    comment added

--- dynamic_class_and_object.py
+++ dynamic_class_and_object.py
@@ -1,10 +1,20 @@
+'''
+    Programme dynamic class and object
+    Author : mAk
+    CopyRight: 2020
+
+'''
 import random
 class Enemy:
+    hp = 200
     def __init__(self,atkl,atkh):
         self.atkl = atkl
         self.atkh = atkh
     def getAtk(self):
         print(self.atkl)
+    def getHp(self):
+        print("Hp is",self.hp)
 
 enm1 = Enemy(10,100)
 enm1.getAtk()
+enm1.getHp()
