23ec0dd12ea73433e7ad50ab705c04cd4fc1fe45
mak <beherapapu44@gmail.com>
2020-03-28 12:23:51 +0530

    playing with while and break with random library
    random.randrange(lowerlimit, higherlimit)

---
+++ enemy.py
@@ -0,0 +1,19 @@
+import random
+
+playerhp = 260
+enemyatkl = 60
+enemyatkh = 80
+
+while playerhp > 0:
+    dmg = random.randrange(enemyatkl,enemyatkh)
+    playerhp = playerhp - dmg
+    if playerhp <= 30:
+        playerhp = 30
+    print("Enemy strikes for", dmg, "Point of damage. Current HP is", playerhp)
+
+    if playerhp == 30:
+        print("Minimum Health ")
+        break
+
+
+
